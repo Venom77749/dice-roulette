9c2805439028961fabd44df4c1e64a521f29a447
Forlirn <mmifincyhb@gmail.com>
2026-06-04 00:24:05 +0500

    Health bar

--- main.gd
+++ main.gd
@@ -5,21 +5,31 @@ var ai_hp: int = 20
 
 enum DiceType { GOOD, BAD }
 
+# Получаем доступ к UI (ТЕКСТ) - пути ведут к узлам Label
+@onready var player_hp_label: Label = $CanvasLayer/PlayerHP
+@onready var ai_hp_label: Label = $CanvasLayer/AIHP
+
+# Получаем доступ к UI (ПОЛОСКИ) - пути ведут к узлам ProgressBar
+@onready var player_hp_bar: ProgressBar = $CanvasLayer/PlayerHealthBar
+@onready var ai_hp_bar: ProgressBar = $CanvasLayer/AIHealthBar
+
 # @onready позволяет получить ссылку на узел RigidBody3D, когда сцена загрузится
 @onready var dice_rigid_body: RigidBody3D = $PhysicsDice
 
 func _ready() -> void:
 	print("--- Игра началась! ---")
 	print("HP Игрока: ", player_hp, " | HP ИИ: ", ai_hp)
+	
+	# Обновляем UI при старте, чтобы полоски сразу заполнились
+	update_ui()
 
-# Подключаем сигнал нажатия кнопки напрямую кодом
+	# Подключаем сигнал нажатия кнопки напрямую кодом
 	$CanvasLayer/Button.pressed.connect(_on_button_pressed)
 	
 func _on_button_pressed() -> void:
 	print("\nИгрок бросает кубик...")
 	
-	
-	# 1. Возвращаем кубик в исходную точку над столом, если он улетел
+	# 1. Возвращаем кубик в исходную точку над столом
 	dice_rigid_body.global_position = Vector3(0, 2, 0)
 	# Сбрасываем старую скорость движения и вращения
 	dice_rigid_body.linear_velocity = Vector3.ZERO
@@ -34,8 +44,17 @@ func _on_button_pressed() -> void:
 	dice_rigid_body.apply_central_impulse(impulse)
 	dice_rigid_body.apply_torque_impulse(torque)
 	
-	# Вызываем нашу логику расчета (пока мгновенную)
+	# Вызываем нашу логику расчета
 	roll_dice(true)
+	
+func update_ui() -> void:
+	# Меняем текст
+	player_hp_label.text = "Здоровье Игрока: " + str(player_hp)
+	ai_hp_label.text = "Здоровье ИИ: " + str(ai_hp)
+	
+	# Обновляем полоски здоровья (их текущее значение)
+	player_hp_bar.value = player_hp
+	ai_hp_bar.value = ai_hp
 
 func roll_dice(is_player: bool) -> void:
 	var value: int = randi_range(1, 6)
@@ -57,6 +76,10 @@ func apply_effect(is_player: bool, value: int, type: DiceType) -> void:
 		
 	print("%s вытянул: %d. Эффект: %s" % [target_name, value, effect_text])
 	print("Текущие HP -> Игрок: %d | ИИ: %d" % [player_hp, ai_hp])
+	
+	# Вызываем обновление интерфейса сразу после изменения HP
+	update_ui()
+	
 	check_win_condition()
 
 func check_win_condition() -> void:
